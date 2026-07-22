3b94f801bb972aae511572217c4eb295e11a4760
D0nLinux <ivan.paredes14@inacapmail.cl>
2026-07-22 03:08:59 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import { Routes, Route } from 'react-router-dom'
+import Inicio from './pages/inicio'
+import Gestion from './pages/gestion'
+import DatosAPI from './pages/datos_api'
+import Barra from './components/barra'
+import Footer from './components/footer'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='container'>
+        <Barra />
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/gestion" element={<Gestion />} />
+          <Route path="/datos_api" element={<DatosAPI />} />
+        </Routes>
+        <Footer />
+      </div>
+    </>
+  )
+}
+
+export default App
